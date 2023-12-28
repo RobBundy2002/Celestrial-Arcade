6ae6d0b842fd8ee518f73890d77accbcf9bc0864
Rob <robbielbundy@gmail.com>
2023-12-28 18:03:04 -0500

    Fixed a few things and started some css for kicks and giggles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
+    return (
+        <div className="App">
+            <div className='top-bar'>
+                <h1 className="title">Checkers</h1>
+            </div>
+        </div>
+    );
 }
+
 export default App;
