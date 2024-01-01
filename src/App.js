c8f7c6ebccfef1549879465067cf657ad011a861
Rob <robbielbundy@gmail.com>
2024-01-01 00:43:31 -0500

    Fixed the Reversion

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ const App = () => {
                         </li>
                     </ul>
                 </nav>
-
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/checkers/*" element={<CheckersBoard />} />
